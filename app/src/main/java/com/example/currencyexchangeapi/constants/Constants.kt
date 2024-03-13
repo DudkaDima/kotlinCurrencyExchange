abff3497642d43856d84238dd121cf4c446d7534
DudkaDima <clout.cobaun@gmail.com>
2024-03-13 12:18:28 +0200

    Fixing constant value for currencies

--- app/src/main/java/com/example/currencyexchangeapi/constants/Constants.kt
+++ app/src/main/java/com/example/currencyexchangeapi/constants/Constants.kt
@@ -6,6 +6,8 @@ import java.math.BigDecimal
 class Constants {
     companion object {
         val EXCHANGE_CURRENCY_FEE:Double = 0.007;
+        val DOLLAR_RATE_TO_EURO = BigDecimal(1.25);
+        val EURO_RATE_TO_DOLLAR = BigDecimal(0.8);
     }
 
 }
--- app/src/main/java/com/example/currencyexchangeapi/currency/CurrenciesToExchangeRate.kt
+++ app/src/main/java/com/example/currencyexchangeapi/currency/CurrenciesToExchangeRate.kt
@@ -1,13 +1,14 @@
 package com.example.currencyexchangeapi.currency
 
+import com.example.currencyexchangeapi.constants.Constants
 import java.math.BigDecimal
 
 
 class CurrenciesToExchangeRate (){
     companion object{
-        val EURO = Currency("EUR", BigDecimal(0.8))
+        val EURO = Currency("EUR", Constants.EURO_RATE_TO_DOLLAR)
 
-        val USDOLLAR = Currency("USD",  BigDecimal(1.25))
+        val USDOLLAR = Currency("USD", Constants.DOLLAR_RATE_TO_EURO)
         fun initCurrencies(): HashMap<String, Currency> {
             var currencies: HashMap<String, Currency> = HashMap();
             currencies.put(this.EURO.getName(), this.EURO);
